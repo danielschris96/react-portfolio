d00206e7212ed966dba15cc85748fab585a56b86
Chris Daniels <danielschris96@gmail.com>
2023-05-28 19:38:02 -0700

    improved css and content for contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ function Contact() {
     return (
       <section className='contact-section'>
           <div className='contact-form-container'>
-              <h2>Contact</h2>
+              <h2>Contact Me</h2>
               <form className='contact-form'>
                   <label htmlFor="name">Name</label>
                   <input type="text" id="name" name="name" required />
@@ -17,12 +17,12 @@ function Contact() {
               </form>
           </div>
           <div className='contact-info'>
-              <h2>Contact Info</h2>
+              <h2>My Info</h2>
               <ul>
-                  <li>Email: danielschris96@gmail.com</li>
-                  <li>Phone: (360) 731-3065</li>
-                  <li>LinkedIn: <a href='https://www.linkedin.com/in/christopher-daniels-01317726b/'>LinkedIn profile</a></li>
-                  <li>Github: <a href='https://github.com/danielschris96'>Github profile</a></li>
+                  <li><a href="mailto:danielschris96@gmail.com">danielschris96@gmail.com</a></li>
+                  <li><a href="tel:3607313065">(360) 731-3065</a></li>
+                  <li><a href='https://www.linkedin.com/in/christopher-daniels-01317726b/'>LinkedIn</a></li>
+                  <li><a href='https://github.com/danielschris96'>Github</a></li>
               </ul>
           </div>
       </section>
