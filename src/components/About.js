09203425ec844c8b631355f7952e0da5701f9496
Chris Daniels <danielschris96@gmail.com>
2023-05-30 22:03:22 -0700

    change to image path

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ function About(props) {
   return (
       <section>
           <h2>About Me</h2>
-          <img src="/portfolio.png" alt="placeholder" className='headshot' />
+          <img src="/react-portfolio/portfolio.PNG" alt="placeholder" className='headshot' />
           <p className='about-text'>
             My name is Chris Daniels and I love working hard. My dream position is the current opening at your company. My hobbies are the same as yours so we'd probably get along and have stuff to talk about if you hired me. Just give me a chance. Please. I need a good job so I can impress my girlfriend's parents. I can't ask them for their blessing in marrying their daughter until I have a solid job that reassures them I will be able to provide for their daughter and their future grandchildren. There's so much riding on the line here. Give me a shot and I promise I'll at least do my best. Thanks.
           </p>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -9,14 +9,14 @@ function Portfolio() {
           description: "Savvy Chef is a user-friendly web application that solves the problem of meal planning by suggesting recipes based on the ingredients available in your kitchen. With its intuitive interface and extensive filtering options, it provides tailored recipes that accommodate dietary restrictions and preferences. Additionally, the app offers cocktail pairing suggestions to complement your meal.",
           link: "https://didriklindberg.github.io/savvy-chef/",
           repo: "https://github.com/DidrikLindberg/savvy-chef",
-          image: "/savvychef.JPG"
+          image: "/react-portfolio/savvychef.JPG"
       },
       {
           name: "Streemy",
           description: "Streemy is an application that aggregates streams from different platforms, allowing users to track their favorite streamers easily. It provides features such as displaying online and offline streamers and the ability to favorite streamers.",
           link: "https://young-tor-26363.herokuapp.com/",
           repo: "https://github.com/kylecoding1/Project2",
-          image: "/streemy.JPG"
+          image: "/react-portfolio/streemy.JPG"
       },
       { 
           name: "Project 3",
