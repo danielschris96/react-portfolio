128f6cdcf68d8905ac2ebcbeefae8bf66d0ad89a
Chris Daniels <danielschris96@gmail.com>
2023-06-27 14:28:58 -0700

    changed about page structure and content

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './About.css';
 
-function About(props) {
+function About() {
   return (
-      <section>
+    <section className='about-wrapper'>
+          <div className='text-wrapper'>
           <img src="/react-portfolio/portfolio.PNG" alt="placeholder" className='headshot' />
+        <h2 className='about-title'>Hey there!</h2>
+          </div>
           <p className='about-text'>
-            My name is Chris Daniels and I love working hard. My dream position is the current opening at your company. My hobbies are the same as yours so we'd probably get along and have stuff to talk about if you hired me. Just give me a chance. Please. I need a good job so I can impress my girlfriend's parents. They call me a deadbeat. Like how am I going to ask for their blessing if I don't have a job? There's so much riding on the line here. Give me a shot and I promise I'll at least do my best. Thanks.
+            I'm Chris Daniels, a dedicated Full Stack Web Developer with a background in biochemistry. I bring a unique blend of analytical thinking and creative coding skills to the table.
+
+            I completed a Full Stack Web Development Boot Camp at UC Berkeley Extension, where I gained hands-on experience in HTML5, CSS, JavaScript, and various frameworks and tools like Bootstrap, jQuery, and Git.
+
+            During my time as a Teaching Assistant and Learning Assistant at Gonzaga University, I honed my communication and teaching abilities while guiding students in chemistry and biology courses.
+
+            With a passion for problem-solving and a detail-oriented mindset, I contributed to a peptide library project as a Research Assistant, gaining proficiency in laboratory techniques.
+
+            Beyond my professional journey, I've volunteered at Camp Oasis, supporting campers with Crohn's disease or ulcerative colitis, and at Sacred Heart Medical Center, providing care in the Pediatric Emergency Department.
+
+            I'm thrilled to bring my diverse background and Full Stack Web Development skills to your team. Let's connect and discuss how I can contribute to your success.
           </p>
       </section>  
   );  
