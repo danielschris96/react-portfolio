dbc258b30e156f6ed0bd58a3c83b4a13487c7b13
Chris Daniels <danielschris96@gmail.com>
2023-06-28 13:27:45 -0700

    added highlight for current component, fixed link to contact

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,41 +1,42 @@
 import React, { useState } from 'react';
 import './Header.css';
 
-function Header(props) {
+function Header({ currentSection, setCurrentSection }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleLogoClick = () => {
-    props.setCurrentSection('AboutMe');
+    setCurrentSection('AboutMe');
   };
 
   const handleLinkClick = (section) => {
-    props.setCurrentSection(section);
+    setCurrentSection(section);
     setIsOpen(false);
   };
 
   return (
     <header>
       <nav>
-      <h1 className='logo' onClick={handleLogoClick}>Chris Daniels</h1>
-<div className="menu-icon" onClick={() => setIsOpen(!isOpen)}>
-  <i className={isOpen ? "fas fa-times" : "fas fa-bars"}></i>
-</div>
-<ul>
-  <li onClick={() => handleLinkClick('About Me')}>About</li>
-  <li onClick={() => handleLinkClick('Portfolio')}>Portfolio</li>
-  <li onClick={() => handleLinkClick('Contact')}>Contact</li>
-  <li onClick={() => handleLinkClick('Resume')}>Resume</li>
-</ul>
-{isOpen && (
-  <ul className={`mobile-nav-list ${isOpen ? 'open' : ''}`}>
-  <li onClick={() => handleLinkClick('About Me')}>About</li>
-  <li onClick={() => handleLinkClick('Portfolio')}>Portfolio</li>
-  <li onClick={() => handleLinkClick('Contact')}>Contact</li>
-  <li onClick={() => handleLinkClick('Resume')}>Resume</li>
-</ul>
-)}
+        <h1 className='logo' onClick={handleLogoClick}>Chris Daniels</h1>
+        <div className="menu-icon" onClick={() => setIsOpen(!isOpen)}>
+          <i className={isOpen ? "fas fa-times" : "fas fa-bars"}></i>
+        </div>
+        <ul>
+          <li className={currentSection === 'AboutMe' ? 'selected' : ''} onClick={() => handleLinkClick('AboutMe')}>About</li>
+          <li className={currentSection === 'Portfolio' ? 'selected' : ''} onClick={() => handleLinkClick('Portfolio')}>Portfolio</li>
+          <li className={currentSection === 'Contact' ? 'selected' : ''} onClick={() => handleLinkClick('Contact')}>Contact</li>
+          <li className={currentSection === 'Resume' ? 'selected' : ''} onClick={() => handleLinkClick('Resume')}>Resume</li>
+        </ul>
+        {isOpen && (
+          <ul className={`mobile-nav-list ${isOpen ? 'open' : ''}`}>
+            <li className={currentSection === 'AboutMe' ? 'selected' : ''} onClick={() => handleLinkClick('AboutMe')}>About</li>
+            <li className={currentSection === 'Portfolio' ? 'selected' : ''} onClick={() => handleLinkClick('Portfolio')}>Portfolio</li>
+            <li className={currentSection === 'Contact' ? 'selected' : ''} onClick={() => handleLinkClick('Contact')}>Contact</li>
+            <li className={currentSection === 'Resume' ? 'selected' : ''} onClick={() => handleLinkClick('Resume')}>Resume</li>
+          </ul>
+        )}
       </nav>
     </header>
   );
 }
+
 export default Header;
